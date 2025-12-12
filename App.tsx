d36f0f0d81747387fe82fcd16c345ddf8b71ab73
김현민 <hyunmins@douzone.com>
2025-12-12 13:45:51 +0900

    add feature

--- App.tsx
+++ App.tsx
@@ -78,8 +78,7 @@ function App(): JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits. TESTTESTfasfas
-            dasdsadasda
+            screen and then come back to see your edits. TESTzz
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
