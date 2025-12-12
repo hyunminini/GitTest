50921729c5479311cbf98dfef2a77c6b877711f0
김현민 <hyunmins@douzone.com>
2025-12-12 14:05:33 +0900

    ff true

--- App.tsx
+++ App.tsx
@@ -87,7 +87,7 @@ function App(): JSX.Element {
             <DebugInstructions />
           </Section>
           <Section title="Learn More">
-            Read the docs to discover what to do next: dasdsadada test
+            Read the docs to discover what to do next: dasdsadada test ㅋㅋ
           </Section>
           <LearnMoreLinks />
         </View>
