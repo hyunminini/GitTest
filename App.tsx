044cbac381510d777a64b4e6d2e293fcc83ed405
김현민 <hyunmins@douzone.com>
2025-12-12 10:56:45 +0900

    chadad

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ function App(): JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits. 테스트 dada
+            screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
